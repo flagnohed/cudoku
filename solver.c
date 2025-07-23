dadf407cbf9b67f96d3c487a09f150b9dc772d0d
Felix <felix@lagnohed.se>
2025-07-23 13:31:06 +0200

    last_free_cell rewrite + remove function that needs to be rewritten

--- solver.c
+++ solver.c
@@ -1,41 +1,39 @@
 // #include "draw.h"  /* Needed for when we want to draw the solution. */
 #include "grid.h"
 // #include <stdio.h>
+
+
 typedef enum {
     BOX,
     COLUMN,
     ROW,
 }   Subset_t;
 
-/* For a subset, check if we have one single empty cell. */
-bool last_free_cell(Cell subset[ROW_LEN]) {
+
+/* For a subset, check if we have one single empty cell.
+ * Returns 0 if no last free cell found, or value between 1-9
+ * if found. */
+int last_free_cell(Cell subset[ROW_LEN]) {
+    int i, found_value;
+    int unseen[ROW_LEN] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int empty_cells = 0;
-    for (int i = 0; i < ROW_LEN; i++) {
-        if (subset[i].value == 0)
+
+    for (i = 0; i < ROW_LEN; i++) {
+        found_value = subset[i].value;
+        if (found_value == 0)
             empty_cells++;
+        else
+            unseen[found_value - 1] = 0;
     }
-    return empty_cells == 1;
+    if (empty_cells != 1)
+        return 0;
+
+    for (i = 0; i < ROW_LEN; i++) {
+        if (unseen[i])
+            return unseen[i];
+    }
+
+    /* Should never reach this. */
+    return 0;
 }
 
-/* Last remaining cell helper function. */
-void lrc_helper(Cell subset[ROW_LEN], Subset_t stype, int idx) {
-    if (stype == ROW) {
-        /* subset is already in the row-format, so
-           just pass it along. */
-        //return is_allowed()
-    }
-
-}
-
-/* For a subset, check if we have exactly one possible
-   location to place a certain value.
-   Returns 0 if no value was found using this method,
-   or the first value matching the requirements. */
-int last_remaining_cell(Cell subset[ROW_LEN], Subset_t stype) {
-    for (int i = 1; i < ROW_LEN + 1; i++) {
-        /* Test all values 1-9. */
-        for (int j = 0; j < ROW_LEN; j++) {
-
-        }
-    }
-}
